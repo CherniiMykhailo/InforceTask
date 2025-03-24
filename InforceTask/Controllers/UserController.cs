3917063f4da2db96b10603ddb0ca1cda6f6d5e6f
Mykhailo Chernii <mykhailo.chernii@gmail.com>
2025-03-24 19:05:29 +0200

    add details page, buttons(details, delete, add)

--- InforceTask/Controllers/UserController.cs
+++ InforceTask/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using InforceTask.Models;
+using InforceTask.Models.Repository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InforceTask.Controllers
@@ -7,9 +9,81 @@ namespace InforceTask.Controllers
     [Route("User")]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly IUrlRepository repository;
+
+        public UserController(IUrlRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [Route("Create")]
+        public ViewResult Create()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        [Route("Create")]
+        public IActionResult Create(string originalUrl)
+        {
+            if (string.IsNullOrWhiteSpace(originalUrl))
+            {
+                ModelState.AddModelError("originalUrl", "The URL cannot be empty.");
+                return View();
+            }
+
+            var existingUrl = repository.Urls.FirstOrDefault(u => u.OriginalUrl == originalUrl);
+            if (existingUrl != null)
+            {
+                ModelState.AddModelError("originalUrl", "This URL already exists in the database.");
+                return View();
+            }
+
+            var currentUser = User.Identity?.Name ?? "Unknown User";
+
+            var newUrl = new Url
+            {
+                OriginalUrl = originalUrl,
+                ShortUrl = GenerateShortUrl(),
+                CreatedBy = currentUser,
+                CreatedDate = DateTime.UtcNow
+            };
+
+            repository.CreateUrl(newUrl);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var url = repository.Urls.FirstOrDefault(u => u.Id == id);
+
+            if (url == null)
+            {
+                return NotFound();
+            }
+
+            return View(url);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var url = repository.Urls.FirstOrDefault(u => u.Id == id);
+
+            if (url == null)
+            {
+                return NotFound();
+            }
+
+            repository.DeleteUrl(url);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private static string GenerateShortUrl()
+        {
+            return Guid.NewGuid().ToString().Substring(0, 8);
         }
     }
 }
